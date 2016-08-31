9949c1922ea0102957b35b2ae3490ff89143152d
Yoda <zoneyoda@gmail.com>
2016-08-31 13:55:27 +0800

    add to github

---
+++ app/src/main/java/com/vitasoy/catint/vitasoy/VitaActivity.java
@@ -0,0 +1,13 @@
+package com.vitasoy.catint.vitasoy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VitaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vita);
+    }
+}
